07a8fde68fa3cff73e79f8c15b5801653113b6e9
Ivan <zhostovivan@gmail.com>
2015-11-23 17:58:31 +0200

    Zombie Apocalypse

---
+++ Assets/Scripts/PointsScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class PointsScript : MonoBehaviour
+{
+    public float points = 0;
+    void Start()
+    {
+       
+       
+        
+    }
+
+
+    void Update()
+    {
+
+    }
+
+}
